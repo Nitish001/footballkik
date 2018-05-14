a68b268e030e994281078d33e7894d7b1fa4d598
kp <nitish001001001@gmail.com>
2018-05-14 15:42:24 +0530

    setting up all the modules

---
+++ controllers/users.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = function(lodash){
+    return{
+      SetRouting : function(router){
+        router.get('/', this.indexPage);
+      },
+      indexPage : function(req ,res){
+        return res.render('index', {test1 : "this is a test page"});
+      }
+    }
+}
